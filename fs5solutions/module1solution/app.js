b96d1554e14b2516cbb54d821d2136449988f302
Ken Ellis <kenellis152@gmail.com>
2017-02-02 21:43:50 -0500

    add angular moduel 1 solution

---
+++ fs5solutions/module1solution/app.js
@@ -0,0 +1,44 @@
+(function () {
+  'use strict';
+
+  angular.module('LunchCheck', [])
+  .controller('LunchCheckController', LunchCheckController);
+
+  LunchCheckController.$inject = ['$scope'];
+  function LunchCheckController($scope){
+
+    $scope.lunchinput = "list comma separated dishes you usually have for lunch";
+    $scope.buttontext = "Check if too much";
+    $scope.output = "waiting for input...";
+    $scope.color = "red";
+    $scope.optionalout ="";
+
+    $scope.process = function(){
+      var inputarray = $scope.lunchinput.split(',');
+      var validcount = inputarray.length;
+
+      for(var i = 0; i < inputarray.length; i++){
+        if(inputarray[i]=="" || inputarray == " "){
+          validcount = validcount - 1;
+          $scope.optionalout += "invalid entry! ";
+        }
+      }
+
+      if(validcount == 0 || $scope.lunchinput == "" || $scope.lunchinput == "list comma separated dishes you usually have for lunch"){
+        $scope.output = "Please enter data first";
+      }
+      else if(inputarray != null){
+          if(validcount <= 3){
+            $scope.output = "Enjoy!";
+            $scope.color = "green";
+
+          }
+          else if(validcount > 3){
+            $scope.output = "Too much!";
+            $scope.color = "green";
+          }
+      }
+    };
+  }
+
+})();
