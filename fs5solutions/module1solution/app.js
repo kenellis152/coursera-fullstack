0f93d9b7530cc910e46dd56a1b364e9823fe61a2
Ken Ellis <kenellis152@gmail.com>
2017-02-02 21:54:20 -0500

    some minor fixes

--- fs5solutions/module1solution/app.js
+++ fs5solutions/module1solution/app.js
@@ -16,22 +16,23 @@
     $scope.process = function(){
       var inputarray = $scope.lunchinput.split(',');
       var validcount = inputarray.length;
-
+      $scope.optionalout = "";
       for(var i = 0; i < inputarray.length; i++){
-        if(inputarray[i]=="" || inputarray == " "){
+        if(inputarray[i]=="" || inputarray[i] == " "){
           validcount = validcount - 1;
-          $scope.optionalout += "invalid entry! ";
+          $scope.optionalout = "invalid entry! ";
+          $scope.color = "red";
         }
       }
 
       if(validcount == 0 || $scope.lunchinput == "" || $scope.lunchinput == "list comma separated dishes you usually have for lunch"){
         $scope.output = "Please enter data first";
+        $scope.color = "red";
       }
       else if(inputarray != null){
           if(validcount <= 3){
             $scope.output = "Enjoy!";
             $scope.color = "green";
-
           }
           else if(validcount > 3){
             $scope.output = "Too much!";
